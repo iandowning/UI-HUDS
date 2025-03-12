f1b52352bcc291671fb15f02f5d36c87e1b78dd8
Ian D <ianmd72@gmail.com>
2025-03-12 11:35:11 -0700

    Score Scene complete
    Have score scene almost done. Just need to add DOTWeen animations

---
+++ Assets/_Scripts/CoinCounterUi.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using TMPro;
+
+public class CoinCounterUi : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+    private float containerInitPosition;
+    private float moveAmount;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
