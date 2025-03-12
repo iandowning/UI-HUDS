9a217e1c7ff9fa8b7b8d0368238af7218468b057
Ian D <ianmd72@gmail.com>
2025-03-12 12:44:24 -0700

    Score UI works correctly

--- Assets/_Scripts/CoinCounterUi.cs
+++ Assets/_Scripts/CoinCounterUi.cs
@@ -12,6 +12,7 @@ public class CoinCounterUi : MonoBehaviour
     private float containerInitPosition;
     private float moveAmount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Ease animationCurve;
     private void Start()
     {
         Canvas.ForceUpdateCanvases();
@@ -19,6 +20,23 @@ public class CoinCounterUi : MonoBehaviour
         toUpdate.SetText("0");
         containerInitPosition = coinTextContainer.localPosition.y;
         moveAmount = current.rectTransform.rect.height;
+    }
+
+
+    public void UpdateScore(int score)
+    {
+        toUpdate.SetText($"{score}");
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetCoinContainer(score));
+    }
+
+        private IEnumerator ResetCoinContainer(int score)
+    {
+        yield return new WaitForSeconds(duration);
+        current.SetText($"{score}"); 
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x,
+        containerInitPosition, localPosition.z);
     }
 
     // Update is called once per frame
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int score = 0;
-    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private CoinCounterUi coinCounter;
 
     protected override void Awake()
     {
@@ -12,10 +12,9 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
     public void IncreaseScore()
     {
         score++;
-        scoreText.text = $"Score: {score}";
+        coinCounter.UpdateScore(score);
     }
 }
